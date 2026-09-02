922b988fa4942958cd4f5d76a27fe28d021f0d16
cowkite202 <25-588@wooshin.sen.hs.kr>
2026-09-02 09:58:22 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const GOOGLE_SCRIPT_URL =
-  "https://script.google.com/macros/s/AKfycbyl6Qq03k4zIjHv8buA_TqtYVy4vLVmeYde2j68LD59XqpKBATQFaetN36EDyMTPLi2mQ/exec";
+  "https://script.google.com/macros/s/AKfycbysXqcNrTxC1_h6vcm6jVCLkMK6Mm41Pu8lsU-LgDZjq6VALYepiZwHQ8LmY-aJRuFOsw/exec";
 
 
 /* =====================================================
